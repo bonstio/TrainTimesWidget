a957d203ccef97a64f2d3adb0e88c3393fc6a6c6
bonstio <github@bonstio.net>
2025-11-27 01:00:31 +0000

    Refresh on title tap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.bonstio.traintimes"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.01"
+        versionCode = 3
+        versionName = "1.03"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
